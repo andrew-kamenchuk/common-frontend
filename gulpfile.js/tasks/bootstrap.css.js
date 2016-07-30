a8aa1259d8ee7a63505ed2e99f439ba1d41e387d
andrew-kamenchuk <andrew.kamenchuk@gmail.com>
2016-07-31 02:03:26 +0300

    dependent tasks

--- gulpfile.js/tasks/bootstrap.css.js
+++ gulpfile.js/tasks/bootstrap.css.js
@@ -7,3 +7,5 @@ module.exports = function() {
     gulp.src(config.src.node + "/bootstrap/dist/{css,fonts}/*")
         .pipe(gulp.dest(config.dest.vendor + "/bootstrap"));
 };
+
+module.exports.deps = ["jquery-bootstrap"];
