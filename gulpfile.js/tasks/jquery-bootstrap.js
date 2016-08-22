5a505a074b7e0e77ef1193fee2b32abf33ebfb5d
andrew-kamenchuk <andrew.kamenchuk@gmail.com>
2016-08-22 07:34:21 +0300

    fix

--- gulpfile.js/tasks/jquery-bootstrap.js
+++ gulpfile.js/tasks/jquery-bootstrap.js
@@ -1,28 +1,28 @@
 "use strict";
 
-let config = require("../../config");
-let gulp = require("gulp");
-let browserifyStr = require("browserify-string");
-let source = require("vinyl-source-stream");
-let uglify = require("gulp-uglify");
-let babelify = require("babelify");
-let buffer = require("gulp-buffer");
+module.exports = (gulp, config) => {
+    const browserifyStr = require("browserify-string");
+    const source = require("vinyl-source-stream");
+    const uglify = require("gulp-uglify");
+    const babelify = require("babelify");
+    const buffer = require("gulp-buffer");
 
-module.exports = () => {
-    let b = browserifyStr(`
-        let $ = global.$ = global.jQuery = require("jquery");
-        require("bootstrap");
-        module.exports = $;
-    `);
+    return () => {
+        const b = browserifyStr(`
+            let $ = global.$ = global.jQuery = require("jquery");
+            require("bootstrap");
+            module.exports = $;
+        `);
 
-    let bundleStream = b
-        .transform(babelify, { presets: ["es2015"] })
-        .require("jquery")
-        .bundle();
+        const bundleStream = b
+            .transform(babelify, { presets: ["es2015"] })
+            .require("jquery")
+            .bundle();
 
-    bundleStream
-        .pipe(source("bootstrap.min.js"))
-        .pipe(buffer())
-        .pipe(uglify())
-        .pipe(gulp.dest(config.dest.vendor + "/bootstrap"));
+        return bundleStream
+            .pipe(source("bootstrap.min.js"))
+            .pipe(buffer())
+            .pipe(uglify())
+            .pipe(gulp.dest(config.dest.vendor + "/bootstrap"));
+    };
 };
